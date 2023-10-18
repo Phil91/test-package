7a8cd3a3b161791275d5d5facc6fc3e55fb32348
Phil Schneider <info@philschneider.de>
2023-10-18 15:20:05 +0200

    chore: add test project

---
+++ src/framework/TestPackage.Framework/StringHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace TestPackage.Framework;
+
+public static class StringHelper
+{
+    /// <summary>
+    /// This method does a nullOrWhiteSpace test and logs the result to the console
+    /// </summary>
+    /// <param name="stringValue">the value that should be checked</param>
+    /// <returns><c>true</c> if the string is null or whitespace otherwise <c>false</c></returns>
+    public static bool IsNullOrWhiteSpaceWithConsoleOutput(this string? stringValue)
+    {
+        var result = string.IsNullOrWhiteSpace(stringValue);
+        Console.WriteLine($"string check is {result}");
+        return result;
+    }
+}
